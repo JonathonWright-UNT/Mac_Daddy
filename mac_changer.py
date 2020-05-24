e70923ae80208c7cc315ef1310787c216d03ae8e
Jonathon Wright <57345504+JonathonWright-UNT@users.noreply.github.com>
2020-05-24 16:31:37 -0500

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -25,18 +25,17 @@ mac_addressEntered.grid(column=1, row=1)
 
 def changeAddress():
 	mac = mac_address.get()
-	banner = ttk.Label(win, text="                          	").grid(column=0, row=2, columnspan=2)
 	if len(mac)==17 and mac[:2].isalnum() and mac[3:5].isalnum() and mac[6:8].isalnum() and mac[9:11].isalnum() and mac[12:14].isalnum() and mac[15:17].isalnum() and (len(re.findall(":", mac)) == 5):
-    	subprocess.run(["ifconfig", interface.get(), "down"])
-    	x = subprocess.run(["ifconfig", interface.get(), "hw", "ether",  mac])
-    	subprocess.run(["ifconfig", interface.get(), "up"])
-    	if (x.returncode == 0):
-        	banner = ttk.Label(win, text="[+] Changing Mac Address for " + interface.get() + " to " + mac_address.get() + "").grid(column=0, row=2, columnspan=2)
+    		subprocess.run(["ifconfig", interface.get(), "down"])
+    		x = subprocess.run(["ifconfig", interface.get(), "hw", "ether",  mac])
+    		subprocess.run(["ifconfig", interface.get(), "up"])
+    		if (x.returncode == 0):
+        		banner = ttk.Label(win, text="[+] Changing Mac Address for " + interface.get() + " to " + mac_address.get() + "").grid(column=0, row=2, columnspan=2)
    	 
-    	else:
-        	banner = ttk.Label(win, text="The Mac Address entered was not acceptable").grid(column=0, row=2, columnspan=2)
+    		else:
+        		banner = ttk.Label(win, text="The Mac Address entered was not acceptable").grid(column=0, row=2, columnspan=2)
 	else:
-    	banner = ttk.Label(win, text="The Mac Address entered was not acceptable").grid(column=0, row=2, columnspan=2)
+    		banner = ttk.Label(win, text="The Mac Address entered was not acceptable").grid(column=0, row=2, columnspan=2)
 	
 def onselect(evt):
 	result = subprocess.run(["ifconfig", interface.get()], capture_output=True)
